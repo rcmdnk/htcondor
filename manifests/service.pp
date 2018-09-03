0c1a51297334a44c821f77830e53ab9797effc2c
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2018-09-03 21:12:59 +0900

    updated for sertice = false

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,18 +1,29 @@
 # Class htcondor::service
 #
 # Starts the services for HTCondor
-class htcondor::service {
-  service { 'condor':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => true,
-  }
+class htcondor::service (
+  $service = true
+){
+  if $service {
+    service { 'condor':
+      ensure     => running,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => true,
+    }
 
-  # this exec is called from the config, but we can't run it if the condor
-  # service is not up. it's a RE-config command assuming something is already up.
-  exec{ '/usr/sbin/condor_reconfig':
-    refreshonly => true,
-    require     => Service['condor']
+    # this exec is called from the config, but we can't run it if the condor
+    # service is not up. it's a RE-config command assuming something is already up.
+    exec{ '/usr/sbin/condor_reconfig':
+      refreshonly => true,
+      require     => Service['condor']
+    }
+  } else {
+    service { 'condor':
+      ensure     => stopped,
+      enable     => false,
+      hasrestart => true,
+      hasstatus  => true,
+    }
   }
 }
